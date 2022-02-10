aee003325ffb542fa4e950a2bcb43db5046cefcb
christian vassallo <chrisslive95@hotmail.com>
2022-02-10 17:01:52 +1100

    Initial commit

---
+++ Getting Into Shapes/Getting_Into_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getting_Into_ShapesApp.swift
+//  Getting Into Shapes
+//
+//  Created by Christian Vassallo on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Getting_Into_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
